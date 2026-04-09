56b120a9d11b2b76d24d738d40c75d9ac9b9b456
Nihal <reddyn2k6@gmail.com>
2026-04-09 17:32:48 +0530

    Folders and files created

---
+++ template.sh
@@ -0,0 +1,16 @@
+#folders
+mkdir -p src
+mkdir -p research
+
+#files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trails.ipynb
+touch requirements.txt
+
+
+echo "Created Successfully"
