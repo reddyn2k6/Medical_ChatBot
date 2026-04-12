2c970de19483fe003cffe09d69119372014c3a8b
Nihal <reddyn2k6@gmail.com>
2026-04-12 09:38:05 +0530

    Made the UI for it

---
+++ app.py
@@ -0,0 +1,87 @@
+from flask import Flask, render_template, jsonify, request
+from src.helper import get_embedding_model
+from langchain_pinecone import PineconeVectorStore
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+from dotenv import load_dotenv
+from src.prompt import *
+import os
+
+
+app = Flask(__name__)
+
+
+load_dotenv()
+
+PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
+HUGGINGFACEHUB_ACCESS_TOKEN=os.environ.get('HUGGINGFACEHUB_ACCESS_TOKEN')
+
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["HUGGINGFACEHUB_ACCESS_TOKEN"] = HUGGINGFACEHUB_ACCESS_TOKEN
+
+
+embeddings = get_embedding_model()
+
+index_name = "medical-chatbot" 
+# Embed each chunk and upsert the embeddings into your Pinecone index.
+docsearch = PineconeVectorStore.from_existing_index(
+    index_name=index_name,
+    embedding=embeddings
+)
+
+
+
+
+retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
+
+llm=HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN")
+)
+
+model=ChatHuggingFace(llm=llm)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+chain = (
+    {
+        "context": retriever | format_docs,
+        "input": RunnablePassthrough()
+    }
+    | prompt
+    | model
+)
+
+
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    response = chain.invoke( msg)
+    print("Response : ", response.content)
+    return str(response.content)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+    
